4e878d65ae2e3b34b06573d1412e912b1a8c8cbe
Matt Collyer <mcollyer@lcs.on.ca>
2021-12-09 14:57:43 -0500

    Initial Commit

---
+++ PhysicsEquationApp/PhysicsEquationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhysicsEquationAppApp.swift
+//  PhysicsEquationApp
+//
+//  Created by Matthew Collyer on 2021-12-09.
+//
+
+import SwiftUI
+
+@main
+struct PhysicsEquationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
